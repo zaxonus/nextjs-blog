e30ed9f8cb97c42e8b423d84995cca21bda3b01a
Michel Bouchet <michelbhe@gol.com>
2023-02-14 16:53:13 +0900

    update nextjs-blog

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Charles de Gaulle est né à Lille le 22 novembre 1890 dans une famille catholique et patriote. Son père, Henri de Gaulle est professeur de lettres et d’histoire.
+Le jeune Charles fait ses études chez les Jésuites et opte très tôt pour la carrière des armes. Il est reçu en 1908 à l’École spéciale militaire de Saint-Cyr, au 119e rang. Avant d’apprendre leur métier d’officier, les élèves doivent connaître la troupe. Charles de Gaulle est donc muté dans un régiment pour y faire l’expérience de son futur métier. Le voilà élève officier au 33e régiment d’infanterie d’Arras, commandé alors par le colonel Pétain. Après ses quatre années d’études à Saint-Cyr, il est à nouveau nommé à Arras en 1912.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
